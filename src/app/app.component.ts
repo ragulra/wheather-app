ceb6a4f274b3bae5269d3b5e1bd9809808278292
ragul <ragul@transity.co>
2020-10-28 18:02:17 +0530

    final update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,NgZone,OnInit } from '@angular/core';
 import {WheatherService} from './wheather.service';
 
 @Component({
@@ -9,20 +9,25 @@ import {WheatherService} from './wheather.service';
 
 export class AppComponent implements OnInit  {                                                                                                                                                                                                                                                                                                                                                         
  
-  constructor(private _weatherService: WheatherService){}
+  constructor(private _weatherService: WheatherService,private zone: NgZone){}
 
     arrayElements: any[] = [];
     items: string[] = [];
     errorMessage: string;
 
     ngOnInit(){
-        for (let index = 0; index < 9; index++){ 
+        for (let index = 0; index < 9; index++)
+        { 
           this.arrayElements.push({'isadd':true, 'isedit': false, 'iserror':false });
         }
+        if(localStorage.getItem("weatherdata"))
+        {
+            this.arrayElements = JSON.parse(localStorage.getItem("weatherdata"));
+        }
+        this.updateWheaterData();
     }
 
    edit(event,index) {
-
     event.preventDefault();
       this.arrayElements.forEach(element => 
       {
@@ -33,28 +38,28 @@ export class AppComponent implements OnInit  {
    }
    
     onClick(event,cityName: string,index){
-    this.errorMessage = "";
-    event.preventDefault();
-
+      if(event){
+        this.errorMessage = "";
+        event.preventDefault();
+      }
     this._weatherService.getWeatheritemsbyCity(cityName).subscribe(data => {
 
-        // var redVal = 255 / (data["main"]["temp_max"] - data["main"]["temp_min"]) * (data["main"]["temp"] - data["main"]["temp_min"]);
-        // var blueVal = 255 / (data["main"]["temp_max"] - data["main"]["temp_min"]) * (data["main"]["temp_max"] - data["main"]["temp"]);
-
-          this.arrayElements[index] = {
-            'city': data["name"],
-            'weatherDescription':data["weather"][0]["description"],
-            'icon':data["weather"][0]["icon"],
-            'temp':data["main"]["temp"],
-            'isedit': true,
-            'isadd':true,
-            'iserror':false,
-          };
+          this.zone.runOutsideAngular(() => {
+            this.arrayElements[index] = {
+              'city': data["name"],
+              'weatherDescription':data["weather"][0]["description"],
+              'icon':data["weather"][0]["icon"],
+              'temp':data["main"]["temp"],
+              'isedit': true,
+              'isadd':true,
+              'iserror':false,
+            };
+          });
+          localStorage.setItem("weatherdata", JSON.stringify(this.arrayElements));
         }, 
         error => 
         {
-          this.arrayElements[index] = 
-          {
+          this.arrayElements[index] = {
             'isedit': false,
             'isadd':true,
             'iserror':true
@@ -63,8 +68,45 @@ export class AppComponent implements OnInit  {
         });
     }
     
-    // getBackgroundColor(arrayitems)
-    // {
-    //  return "rgb(" + arrayitems.redcolor + ", 0," + arrayitems.bluecolor +")"
-    // }
+    getBackgroundimage(arrayitems){
+      if(arrayitems.weatherDescription  == "clear sky" || arrayitems.weatherDescription  == "few clouds"  || arrayitems.weatherDescription == "broken clouds")
+      {
+        return 'skyPg' 
+      }
+      else if(arrayitems.weatherDescription == "shower rain" || arrayitems.weatherDescription == "rain" || arrayitems.weatherDescription == "heavy intensity rain" || arrayitems.weatherDescription == 'light rain' || arrayitems.weatherDescription == "light intensity drizzle")
+      {
+        return 'rainPg'
+      }
+      else if(arrayitems.weatherDescription == "thunderstorm")
+      {
+        return 'thunderPg'
+      }
+      else if(arrayitems.weatherDescription == "snow")
+      {
+        return 'snowPg'
+      }
+      else if(arrayitems.weatherDescription == "overcast clouds" || arrayitems.weatherDescription  == "scattered clouds")
+      {
+        return 'overcastPg'
+      }
+      else if(arrayitems.weatherDescription == "mist")
+      {
+        return 'windPg'
+      }
+    }
+
+    updateWheaterData()
+    {
+      setInterval(() => {
+        if(localStorage.getItem("weatherdata")){
+          let weatherData = JSON.parse(localStorage.getItem("weatherdata"));
+          for(let index = 0; index < weatherData.length; index++)
+          {
+            if(weatherData[index]["isedit"]){
+              this.onClick(null,weatherData[index]["city"],index);
+            }
+          }
+        }
+      },30000);
+    }
 }
